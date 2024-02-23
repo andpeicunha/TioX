3c692003b081ee726c92316670b52011677a6dcc
Andre Peixoto <andrepeixoto@gmail.com>
2024-02-23 09:52:06 -0300

    style: css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Button } from '.'
+import { Button } from ".";
 
 function App() {
-	return (
-		<div className='card m-10'>
-			<Button iconName='done'>Teste</Button>
-			<button>Teste</button>
-		</div>
-	)
+  return (
+    <div className="card m-10">
+      <Button iconName="done">Teste</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/theme/color/index.ts
+++ src/theme/color/index.ts
@@ -1,16 +1,16 @@
 export const color = {
-	primary: {
-		lightest: '#d2fbfb',
-		light: '#71ecef',
-		base: '#15c5ce',
-		dark: '#177a87',
-		darkest: '#1b525e',
-	},
-	secondary: {
-		lightest: '#ffe2d4',
-		light: '#ff8156',
-		base: '#fc3613',
-		dark: '#c51009',
-		darkest: '#7e1010',
-	},
-}
+  primary: {
+    lightest: "var(--primary-lightest)",
+    light: "var(--primary-light)",
+    base: "var(--primary-base)",
+    dark: "var(--primary-dark)",
+    darkest: "var(--primary-darkest)",
+  },
+  secondary: {
+    lightest: "var(--primary-lightest)",
+    light: "var(--primary-light)",
+    base: "var(--primary-base)",
+    dark: "var(--primary-dark)",
+    darkest: "var(--primary-darkest)",
+  },
+};
